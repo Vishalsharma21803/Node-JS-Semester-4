822e8dda5b1b98dcbad8e56080b0411ef39d0e18
Vishal Sharma <vishal2003ggg@gmail.com>
2024-01-18 20:04:56 +0530

    new code

--- myfirst.js
+++ myfirst.js
@@ -1,12 +1,12 @@
-// var http = require('http');
+var http = require('http');
 
-// http.createServer(function (req, res) {
-//   res.writeHead(200, {'Content-Type': 'text/plain'});
-//   res.end('Hello World!');
-// }).listen(8085);
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World!');
+}).listen(8085);
 
-// console.log('Vishal Sharma');
-// console.error("Error!");
-// const name="vishal";
-// console.warn(`Dont mess with me ${name}!` );
+console.log('Vishal Sharma');
+console.error("Error!");
+const name="vishal";
+console.warn(`Dont mess with me ${name}!` );
 console.log("hello world");
